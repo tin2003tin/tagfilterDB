e046b5f07009d3ff9f0195a6b2a67837799a85bf
tin2003tin <tinsiriwid@gmail.com>
2024-10-31 10:30:43 +0700

    Add PQ for SQL

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include "json.hpp"
 #include "tagfilterdb/service1.hpp"
 #include "tagfilterdb/logging.hpp"
+#include <iostream>
+#include <libpq-fe.h>
 
 using json = nlohmann::json;
 
@@ -12,14 +14,88 @@ void set_cors_headers(httplib::Response &res) {
     res.set_header("Access-Control-Allow-Headers", "Content-Type"); // Allowed headers
 }
 
+const char* conninfo = "host=127.0.0.1 port=5433 dbname=banking user=admin password=root";
+
+class BankingDatabase {
+    PGconn* conn;
+    public:
+    BankingDatabase() : conn(nullptr) {}
+    bool Connect(const std::string& conninfo) {
+        conn = PQconnectdb(conninfo.c_str()); 
+
+        if (PQstatus(conn) != CONNECTION_OK) {
+            std::cerr << "Connection to database failed: " << PQerrorMessage(conn) << std::endl;
+            return false;
+        }
+
+        std::cout << "Connected to the database successfully!" << std::endl;
+        return true;
+    }
+   PGresult* GetCustomer(const std::string& name) {
+    std::string query = "SELECT * FROM customer AS c "
+                        "NATURAL JOIN depositor AS d  "
+                        "NATURAL JOIN account AS a   "
+                        "WHERE c.customer_name = '" + name + "';";
+
+    PGresult* res = PQexec(conn, query.c_str());
+
+    if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+        std::cerr << "Query execution failed: " << PQerrorMessage(conn) << std::endl;
+        PQclear(res);
+        return nullptr;
+    }
+
+    return res;
+    }
+
+    static void Print(PGresult* res) {
+        if (res == nullptr) {
+            std::cerr << "No account data to display." << std::endl;
+            return;
+        }
+
+        int nFields = PQnfields(res);
+        int nRows = PQntuples(res);
+
+        for (int i = 0; i < nFields; i++) {
+            std::cout << PQfname(res, i) << "\t";
+        }
+        std::cout << std::endl;
+
+        for (int i = 0; i < nRows; i++) {
+            for (int j = 0; j < nFields; j++) {
+                std::cout << PQgetvalue(res, i, j) << "\t";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    void Disconnect() {
+        if (conn) {
+            PQfinish(conn);
+            conn = nullptr;
+        }
+    }
+
+     ~BankingDatabase() {
+        Disconnect();
+    }
+};
+
 int main() {
     using namespace httplib;
-    // HTTP Server
+
     Server svr;
 
-    // Route to return a simple greeting message in JSON format
-    svr.Get("/hi", [](const Request &, Response &res) {
-        set_cors_headers(res);  // Set CORS headers
+    BankingDatabase db;
+    if (!db.Connect("host=127.0.0.1 port=5433 dbname=banking user=admin password=root")) {
+            std::cerr << "Database connection failed." << std::endl;
+            return 1;
+    }
+    tin_compiler::CompilerService cs;
+
+    svr.Get("/", [](const Request &, Response &res) {
+        set_cors_headers(res); 
         json response_json = {
             {"message", "Hello World!"}
         };
@@ -27,39 +103,40 @@ int main() {
         res.set_content(response_json.dump(), "application/json");
     });
 
-    // Route to return numbers as JSON
-    svr.Get(R"(/numbers/(\d+))", [&](const Request& req, Response& res) {
-        set_cors_headers(res);  // Set CORS headers
-        auto numbers = req.matches[1];
-        json response_json = {
-            {"number", numbers}
-        };
+   svr.Get("/customer/:name", [&](const Request& req, Response& res) {
+        set_cors_headers(res);
+        auto customer_name = req.path_params.at("name");
         
-        res.set_content(response_json.dump(), "application/json");
+        PGresult* res_db = db.GetCustomer(customer_name);  
+        if (res_db) {
+            json response_json;
+            int nRows = PQntuples(res_db);
+            int nFields = PQnfields(res_db);
+
+            for (int i = 0; i < nRows; ++i) {
+                json customer_json;
+                for (int j = 0; j < nFields; ++j) {
+                    customer_json[PQfname(res_db, j)] = PQgetvalue(res_db, i, j);
+                }
+                response_json.push_back(customer_json);
+            }
+            PQclear(res_db);
+            res.set_content(response_json.dump(), "application/json");
+        } else {
+            res.status = 404;
+            res.set_content("{\"error\": \"Customer not found\"}", "application/json");
+        }
     });
 
-    // Route to return user ID as JSON
-    svr.Get("/users/:id", [&](const Request& req, Response& res) {
-        set_cors_headers(res);  // Set CORS headers
-        auto user_id = req.path_params.at("id");
-        json response_json = {
-            {"user_id", user_id}
-        };
-      
-        res.set_content(response_json.dump(), "application/json");
-    });
-
-    svr.Get("/compiler/details", [](const Request &, Response &res) {
-         set_cors_headers(res);  // Set CORS headers
-        tin_compiler::CompilerService cs;
+    svr.Get("/compiler/details", [&](const Request &, Response &res) {
+        set_cors_headers(res);
         json response_json;
         response_json["Detail"] = cs.toJson();
        
         res.set_content(response_json.dump(), "application/json");
     });
 
-    // Route to return body and parameters in JSON format
-    svr.Post("/compiler/plainText", [](const Request& req, Response& res) {
+    svr.Post("/compiler/plainText", [&](const Request& req, Response& res) {
         set_cors_headers(res);
 
         json response_json;
